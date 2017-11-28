e42fd265094d406a9c6cf212688b7ba9e46223e7
johnytiago <joaosao10@gmail.com>
2017-11-28 17:56:51 +0000

    New website :tada:

---
+++ newPage/assets/contact-form/php/form-process.php
@@ -0,0 +1,56 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "É necessário preencher o Nome";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "É necessário preencher o Email";
+} else {
+    $email = $_POST["email"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "É necessário preencher a Mensagem";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "geral@3rconsulting.pt";
+$Subject = "Formulário de Contacto";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
